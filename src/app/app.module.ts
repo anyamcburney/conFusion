f641d03cdefe1a56f818b2d0c005c6ca1d17be36
Anna Mcburney <anya.mcburney@gmail.com>
2018-02-11 11:10:32 -0800

    add modules, load angular/materials, animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 import { NgModule } from '@angular/core';
-
-
+import {FormsModule} from "@angular/forms"
+import {HttpModule} from "@angular/http"
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatToolbarModule
+  // and so on...
+} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout"
 import { AppComponent } from './app.component';
 
-
+import 'hammerjs'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatToolbarModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
