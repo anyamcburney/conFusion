43807dfaded20074043dd7063fe2acd8958520b6
Anna Mcburney <anya.mcburney@gmail.com>
2018-02-11 18:40:30 -0800

    1st assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import {FlexLayoutModule} from "@angular/flex-layout"
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component'
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent, ValuesPipe } from './menu/dishdetail/dishdetail.component'
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    ValuesPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/menu/dishdetail/dishdetail.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, Input, Pipe, PipeTransform } from '@angular/core';
+import {Dish} from '../../shared/dish'
+
+@Pipe({ name: 'values',  pure: false })
+export class ValuesPipe implements PipeTransform {
+  transform(value: any, args: any[] = null): any {
+    return Object.keys(value).map(key => value[key]);
+  }
+}
+const DISH = {
+  name: 'Uthappizza',
+  image: '/assets/images/uthappizza.png',
+  category: 'mains',
+  label: 'Hot',
+  price: '4.99',
+  description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+  comments: [
+    {
+      rating: 5,
+      comment: "Imagine all the eatables, living in conFusion!",
+      author: "John Lemon",
+      date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+      rating: 4,
+      comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+      author: "Paul McVites",
+      date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+      rating: 3,
+      comment: "Eat it, just eat it!",
+      author: "Michael Jaikishan",
+      date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+      rating: 4,
+      comment: "Ultimate, Reaching for the stars!",
+      author: "Ringo Starry",
+      date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+      rating: 2,
+      comment: "It's your birthday, we're gonna party!",
+      author: "25 Cent",
+      date: "2011-12-02T17:57:28.556094Z"
+    }
+  ]
+};
+
+@Component({
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss']
+})
+export class DishdetailComponent implements OnInit {
+ dish = DISH;
+// dish=Dish;
+  @Input() selectedDish: Dish;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
