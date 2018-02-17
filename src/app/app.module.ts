07f87cd45a24c479a67b272eb555e6a2c3c85466
Anna Mcburney <anya.mcburney@gmail.com>
2018-02-16 16:51:33 -0800

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent, ValuesPipe } from './menu/dishdetail/dishdetail.component'
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
-    ValuesPipe
+    ValuesPipe,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
