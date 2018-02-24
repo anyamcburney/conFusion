2678a4784baf14280333449c9a7c2b6090348d77
Anna Mcburney <anya.mcburney@gmail.com>
2018-02-24 10:32:56 -0800

    Single Page Applications Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Pipe, PipeTransform } from '@angular/core';
 import {Dish} from '../shared/dish'
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Pipe({ name: 'values',  pure: false })
 export class ValuesPipe implements PipeTransform {
@@ -15,11 +18,19 @@ export class ValuesPipe implements PipeTransform {
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input() dish: Dish;
+  dish: Dish;
 
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
